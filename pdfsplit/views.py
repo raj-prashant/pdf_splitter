293ae4795f2f21577fa536f4f6360dd681744dc1
Prashant Raj <91521534+raj-prashant@users.noreply.github.com>
2023-10-22 23:23:45 +0530

    Add files via upload

---
+++ pdfsplit/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,HttpResponse
+import os
+from PyPDF2 import PdfReader, PdfWriter
+def extract_pages_to_single_pdf(input_pdf, output_pdf, desired_pages, error):
+    # Convert the input and output paths to absolute paths
+    # input_pdf = os.path.abspath(input_pdf)
+    # output_pdf = os.path.abspath(output_pdf)
+
+    # Open the input PDF file using PdfReader
+    pdf = PdfReader(input_pdf)
+
+    # Create a PdfWriter to collect the desired pages into a single PDF
+    pdf_writer = PdfWriter()
+
+    # Iterate through the desired pages and add them to the PdfWriter
+    for page_num in desired_pages:
+        if page_num+error < 1 or page_num+error > len(pdf.pages):
+            continue  # Skip invalid page numbers
+        pdf_writer.add_page(pdf.pages[page_num+error - 1])  # Page numbers are 0-based
+
+    # Write the collected pages to the output PDF
+    with open(output_pdf, 'wb') as output_file:
+        pdf_writer.write(output_file)
+
+# Create your views here.
+def split(request):
+    if request.method=="POST":
+        error=request.POST.get('error')
+        error=int(error)
+        numb=request.POST.get('numb')
+        input_pdf_file = request.FILES["inputFile"]
+        # document=FilesUpload.objects.create(file=input_pdf_file)
+        # document.save()
+        # return HttpResponse("Uploaded")
+        output_pdf_file = './static/output.pdf'
+        desired =[int(x) for x in numb.split(",")]
+        extract_pages_to_single_pdf(input_pdf_file, output_pdf_file, desired,error)
+        variables={
+            "error":error,
+            "numb":numb,
+        }
+        print(error,numb)
+        print(type(numb[0]))
+        return render(request,'out.html',variables)
+    return render(request,'form.html')
